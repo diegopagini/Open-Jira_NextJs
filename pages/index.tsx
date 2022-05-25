2a0651450224225c56db49938bb03ba95ab80c50
Diego Pagini <diego.pagini@neoris.com>
2022-05-25 15:50:24 +0200

    cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,49 @@
 /** @format */
-import { Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 
 import { Layout } from '../components/layouts';
 
 import type { NextPage } from 'next';
+
 const HomePage: NextPage = () => {
 	return (
-		<Layout title='OpenJira - App'>
-			<Typography variant='h1' color='primary'>
-				Hola mundo
-			</Typography>
+		<Layout title='Home - OpenJira'>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={4}>
+					<Card
+						sx={{
+							height: 'calc(100vh - 100px)',
+						}}
+					>
+						<CardHeader title='Pendientes' />
+
+						<CardContent>
+							{/* Agregar */}
+							{/* Listado */}
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={12} sm={4}>
+					<Card
+						sx={{
+							height: 'calc(100vh - 100px)',
+						}}
+					>
+						<CardHeader title='En Progreso' />
+					</Card>
+				</Grid>
+
+				<Grid item xs={12} sm={4}>
+					<Card
+						sx={{
+							height: 'calc(100vh - 100px)',
+						}}
+					>
+						<CardHeader title='Completadas' />
+					</Card>
+				</Grid>
+			</Grid>
 		</Layout>
 	);
 };
