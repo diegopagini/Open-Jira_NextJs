f405dae0610442cbcb720fe385dcd1e51e98a971
Diego Pagini <diego.pagini@neoris.com>
2022-05-27 13:19:40 +0200

    mongoose

--- database/db.ts
+++ database/db.ts
@@ -13,7 +13,7 @@ const mongooConnection = {
 
 export const connect = async () => {
 	if (mongooConnection.isConnected) {
-		console.log('Ya estamos conectados');
+		console.log('Ya estabamos conectados');
 		return;
 	}
 
@@ -28,13 +28,13 @@ export const connect = async () => {
 		await mongoose.disconnect();
 	}
 
-	await mongoose.connect('....');
+	await mongoose.connect(process.env.MONGO_URL || '');
 	mongooConnection.isConnected = 1;
-	console.log('Conectado a MongoDB:', '....');
+	console.log('Conectado a MongoDB:', process.env.MONGO_URL);
 };
 
 export const disconnect = async () => {
-	if (mongooConnection.isConnected !== 0) return;
+	if (mongooConnection.isConnected === 0) return;
 	await mongoose.disconnect();
 	console.log('Desconectado de MongoDB');
 };
--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,14 +1,22 @@
 /** @format */
+import { db } from '../../database';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
-
 type Data = {
-	name: string;
+	message: string;
 };
 
-export default function handler(
+export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	res.status(200).json({ name: 'Example' });
+	if (process.env.NODE_ENV === 'production') {
+		return res.status(401).json({ message: 'No tiene acceso a este servicio' });
+	}
+
+	await db.connect();
+
+	await db.disconnect();
+
+	res.status(200).json({ message: 'Proceso realizado correctamente' });
 }
